cbf5c4a64ddbccde97202a90a151dc3aa5ba5163
Dubne <nikitaoprysko06@gmail.com>
2026-09-07 15:34:16 +0000

    some cosmetic changes in the settings

--- TeamFlowAPI/settings.py
+++ TeamFlowAPI/settings.py
@@ -10,39 +10,43 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/6.1/ref/settings/
 """
 
+AUTH_USER_MODEL = 'users.User'
+
 from pathlib import Path
 import os
 import environ
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/6.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
+
 env = environ.Env()
 environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 
 SECRET_KEY = env('SECRET_KEY')
 
-# SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env('DEBUG')
 
 ALLOWED_HOSTS = []
 
 
-# Application definition
-
-INSTALLED_APPS = [
+OTHERS_APPS = [
+    'rest_framework'
+]
+DJANGO_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'django.contrib.staticfiles',
-    'rest_framework'
+    'django.contrib.staticfiles'
 ]
+LOCAL_APPS = [
+    'apps.users'
+]
+INSTALLED_APPS = OTHERS_APPS + DJANGO_APPS + LOCAL_APPS
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
