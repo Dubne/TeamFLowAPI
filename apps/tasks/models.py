c98fb42225e9ebdea22868e495833a229bedaa1a
Dubne <nikitaoprysko06@gmail.com>
2026-09-09 12:06:34 +0000

    Creating tag, attachment and comment models

--- apps/tasks/models.py
+++ apps/tasks/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.conf import settings
 from apps.projects.models import Project 
 from apps.teams.models import Team
+
 class Task(models.Model):
     class Status(models.TextChoices):
         OPEN = "OPEN", 'open'
@@ -24,6 +25,35 @@ class Task(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     sla_due_at = models.DateTimeField(null=True, blank=True)
+
+class Comment(models.Model):
+    class Visibility(models.TextChoices):
+        PUBLIC = 'PUBLIC', "Public"
+        INTERNAL = "INTERNAL", "Internal"
+    visibility = models.CharField(max_length=50, choices=Visibility.choices, default=Visibility.PUBLIC)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.SET_NULL, null=True)
+    task = models.ForeignKey(Task, on_delete=models.CASCADE, related_name="comments")
+    text = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+class Tag(models.Model):
+    name = models.CharField(max_length=50)
+    team = models.ForeignKey(Team, on_delete=models.CASCADE, related_name="tags")
+    tasks = models.ManyToManyField(Task, blank=True, related_name="tags")
+    class Meta:
+        constraints =[
+            models.UniqueConstraint(fields=["team", "name"], name="unique_tag_per_team")
+        ] 
+
+class Attachment(models.Model):
+    task = models.ForeignKey(Task, on_delete=models.CASCADE, related_name="attachments")
+    uploaded_by = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.SET_NULL, null=True)
+    file = models.FileField(upload_to="attachments/%Y/%m/")
+    filename = models.CharField(max_length=255)
+    size = models.PositiveIntegerField()
+    content_type = models.CharField(max_length=100)
+    created_at = models.DateTimeField(auto_now_add=True)
 
 
     
