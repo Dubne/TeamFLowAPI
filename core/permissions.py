fb6a1ba70fbd77a4872b4e177a1ef6d3d435b7f6
Dubne <nikitaoprysko06@gmail.com>
2026-09-16 20:53:59 +0000

    Rewrite all permissions because I create services safe enough and do not need bunch of permissions

--- core/permissions.py
+++ core/permissions.py
@@ -2,33 +2,53 @@ from rest_framework.permissions import BasePermission, SAFE_METHODS
 from apps.teams.models import TeamMembership
 from apps.projects.models import Project
 from apps.tasks.models import Task
-def get_team(obj):
-    team = getattr(obj, "team", None)
-    if team is None:
-        return obj
-    return team
+
 
 class IsTeamMember(BasePermission):
     def has_object_permission(self, request, view, obj):
-        team = get_team(obj)
-        return TeamMembership.objects.filter(user=request.user, team=team).exists
+        return TeamMembership.objects.filter(user=request.user, team=obj).exists()
 
-class IsTeamOwner(BasePermission):
+class IsProjectTeamMember(BasePermission):
     def has_object_permission(self, request, view, obj):
-        return TeamMembership.objects.filter(user=request.user, team=obj, role="OWNER").exists
+        return TeamMembership.objects.filter(user=request.user, team=obj.team).exists()
 
-class IsTeamAdmin(BasePermission):
+class IsTaskTeamMember(BasePermission):
     def has_object_permission(self, request, view, obj):
-        return TeamMembership.objects.filter(user=request.user, team=obj, role__in=["OWNER", "ADMIN"]).exists
+        return TeamMembership.objects.filter(user=request.user, team=obj.project.team).exists()
 
-class IsProjectMember(IsTeamMember):
-    pass
-
-class IsTaskCreator(BasePermission):
+class IsCommentTeamMember(BasePermission):
     def has_object_permission(self, request, view, obj):
-        return obj.creator == request.user
+        return TeamMembership.objects.filter(user=request.user, team=obj.task.project.team).exists()
 
-class IsTaskAssignee(BasePermission):
-    def has_object_permission(self, request, view, obj):
-        return obj.assignee == request.user
+
+# def get_team(obj):
+#     team = getattr(obj, "team", None)
+#     if team is None:
+#         return obj
+#     return team
+
+# class IsTeamMember(BasePermission):
+#     def has_object_permission(self, request, view, obj):
+#         team = get_team(obj)
+#         return TeamMembership.objects.filter(user=request.user, team=team).exists()
+
+# class IsTeamOwner(BasePermission):
+#     def has_object_permission(self, request, view, obj):
+#         return TeamMembership.objects.filter(user=request.user, team=obj, role="OWNER").exists()
+
+# class IsTeamAdmin(BasePermission):
+#     def has_object_permission(self, request, view, obj):
+#         return TeamMembership.objects.filter(user=request.user, team=obj, role__in=["OWNER", "ADMIN"]).exists()
+
+# class IsProjectMember(IsTeamMember):
+#     pass
+
+# class IsTaskCreator(BasePermission):
+#     def has_object_permission(self, request, view, obj):
+#         return obj.creator == request.user
+
+# class IsTaskAssignee(BasePermission):
+#     def has_object_permission(self, request, view, obj):
+#         return obj.assignee == request.user
+
 
